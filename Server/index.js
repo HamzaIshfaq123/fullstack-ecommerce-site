644fc33df648836f3f59bec959717c6d65e7a991
Hamza Ishfaq <hamzaishfaq472@gmail.com>
2026-02-01 20:09:51 +0500

    vercel deployment commits

--- Server/index.js
+++ Server/index.js
@@ -4,17 +4,31 @@ const User = require("./src/models/user.model"); // Import to use in routes
 const Product = require("./src/models/product.model")
 
 const app = express();
+
+require('dotenv').config();
+
 connectDB(); // Execute the connection
 
 app.use(express.json());
 
 const cors = require("cors")
 
+// previous code for local connection
+// app.use(
+//     cors({
+//         origin: ['http://localhost:5173'],
+//     })
+// )
+
+// new code for mongodb cluster
 app.use(
     cors({
-        origin: ['http://localhost:5173'],
+        // Add your Vercel frontend URL here
+        origin: ['http://localhost:5173', 'https://fullstack-ecommerce-site-drab.vercel.app/'],
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true
     })
-)
+);
 
 // READ (Get all users)
 app.get("/users", async (req, res) => {
@@ -50,4 +64,14 @@ app.get("/api/products", async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+// commenting this old line for local env out
+//  app.listen(3000, () => console.log("Server running on port 3000"));
+
+// new code for vercel 
+// This allows local testing but won't crash on Vercel
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(3000, () => console.log("Server running on port 3000"));
+}
+
+// You MUST export the app for Vercel to pick it up
+module.exports = app;
--- Server/src/config/db.js
+++ Server/src/config/db.js
@@ -1,12 +1,25 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
+
+// const connectDB = async () => {
+//     try {
+//         await mongoose.connect("mongodb://127.0.0.1:27017/project-ecommerce");
+//         console.log("MongoDB Connected Successfully!");
+//     } catch (err) {
+//         console.error("Error:", err);
+//         process.exit(1);
+//     }
+// };
+
+// module.exports = connectDB;
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/project-ecommerce");
-        console.log("MongoDB Connected Successfully!");
+        // Use process.env to pull the string from Vercel's settings
+        const conn = await mongoose.connect(process.env.MONGODB_URI);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (err) {
-        console.error("Error:", err);
-        process.exit(1);
+        console.error("MongoDB Connection Error:", err);
+        // On Vercel, we don't use process.exit(1) as it can crash the serverless function
     }
 };
 
